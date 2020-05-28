61ebb8569fdc30dd176645dbc97cbda8c75ff23c
EddieMa <machaoxue@yeah.net>
2020-05-28 20:20:31 +0800

    取消提交脚本

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
 set -e
-
-#yarn build
-
+yarn build
 cd dist
 git init
 git add .
 git commit -m 'deploy'
-
 git push -f git@github.com:mcx2020/accounting.git master:gh-pages
 git push -f git@gitee.com:mcx2020/accounting.git master
-
 cd ..
