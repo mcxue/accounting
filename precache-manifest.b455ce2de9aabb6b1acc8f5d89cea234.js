d08488e612c3009e95cfccb1911faf4018badd46
EddieMa <machaoxue@yeah.net>
2020-06-08 21:56:53 +0800

    deploy

---
+++ precache-manifest.b455ce2de9aabb6b1acc8f5d89cea234.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0dec503494aa37403b7",
+    "url": "/accounting/css/app.56e8c6eb.css"
+  },
+  {
+    "revision": "fecf56bf890544fab31bd3dd7c00468d",
+    "url": "/accounting/img/my-favicons/favicon-16px.ico"
+  },
+  {
+    "revision": "61cb5ba3b0180460ee00c8e4b07bd442",
+    "url": "/accounting/img/my-favicons/favicon-32px.ico"
+  },
+  {
+    "revision": "8423e040fd08ee18848eb365871e64e1",
+    "url": "/accounting/index.html"
+  },
+  {
+    "revision": "e0dec503494aa37403b7",
+    "url": "/accounting/js/app.74776ff4.js"
+  },
+  {
+    "revision": "56a4b6f9eee12b4603ca",
+    "url": "/accounting/js/chunk-vendors.69d73aa4.js"
+  },
+  {
+    "revision": "82844b3db80541424433005f4f6bc36e",
+    "url": "/accounting/manifest.json"
+  },
+  {
+    "revision": "070d6eae1c5a2b3ef11d9d12f34c096b",
+    "url": "/accounting/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounting/robots.txt"
+  }
+]);
